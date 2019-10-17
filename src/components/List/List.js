896975b04983eb834c0114a9811d5b5f28c6c0a6
Ola Siem <info@siemieniuk.net>
2019-10-17 16:28:44 +0200

    First props and prop-types install

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -3,13 +3,16 @@ import styles from './List.scss';
 import Hero from '../Hero/Hero.js';
 
 class List extends React.Component {
-    render() {
-        return (
-            <section className={styles.component}>
-                <Hero />
-            </section>
-        )
-    }
+  render() {
+    return (
+      <section className={styles.component}>
+        <Hero titleText={this.props.title} />
+        <div className={styles.description}>
+          {this.props.children}
+        </div>
+      </section>
+    )
+  }
 }
 
 export default List;
