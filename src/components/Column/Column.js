e57d8acd399d1e2225089b72c862cee876b23584
Ola Siem <info@siemieniuk.net>
2019-10-24 16:04:45 +0200

    Add creator component

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -10,7 +10,7 @@ class Column extends React.Component {
   render() {
     return (
       <section className={styles.component}>
-        <h3 className={styles.title}>{this.props.titleColumn}</h3>
+        <h3 className={styles.title}>{this.props.title}</h3>
       </section>
     )
   }  
