0ccb79c920287ca0910da0c86000b37f506513ba
Ola Siem <info@siemieniuk.net>
2019-11-04 10:02:27 +0100

    Add Info component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import Home from '../Home/HomeContainer';
+import Info from '../Info/Info';
 
 const App = () => (
-  <div>
-    <Home />
-  </div>
+  <BrowserRouter>
+    <Switch>
+      <Route exact path='/' component={Home} />
+      <Route exact path='/info' component={Info} />
+    </Switch>
+  </BrowserRouter>
 );
 
 export default App;
